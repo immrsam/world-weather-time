13f7598b403ff8e8ef2265b0cb2bf8be96804709
Sam Bradbury <sam.bradbury@hotmail.com>
2023-01-31 20:48:45 +1100

    initial commit, basic search setup

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "97fe31f750msh78db3c7da2d0d3fp1926a7jsn473f0ce2a8de",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
